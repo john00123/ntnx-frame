0a48ca45966a45f236360549bdae0fd7cbe6a014
John Torres <john.torres.hurtado@gmail.com>
2018-10-25 21:48:39 -0700

    added more icons

--- js/script.js
+++ js/script.js
@@ -66,6 +66,13 @@ function cardIcons(){
         <h3>appTitle</h3>
       </div>`)
   }
+  for (i=1; i<5; i++){
+    $('.Applications .card-body').append(`
+      <div class='iconContainer'>
+        <img src='../img/icon${i}.png'/>
+        <h3>appTitle</h3>
+      </div>`)
+  }
 };
 
 
